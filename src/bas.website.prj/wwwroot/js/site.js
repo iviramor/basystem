325648a171b039f7c1991365e8d3873aca51f1e3
Daniil <iviramor.di@gmail.com>
2021-02-26 01:51:19 +0300

    added html-css-js for calc

--- src/bas.website.prj/wwwroot/js/site.js
+++ src/bas.website.prj/wwwroot/js/site.js
@@ -1,1 +1,24 @@
 ﻿
+
+$('.currency-button').on('click', function(e){
+    $('.currency-button').removeClass('currency-active');
+    $('.currency-button').attr('data-currency-active', 'false');
+    $(this).addClass('currency-active');
+    $(this).attr('data-currency-active', 'true')
+});
+
+
+$('.indiv-percent').on('click', function(e){
+
+    var sel;
+    sel = parseInt($('.calc-select').val())
+
+    console.log(typeof sel)
+
+    $(this).parent('.calc-to-block').children('.calc-input').val((9.3 - sel).toFixed(1));
+    $(this).parent('.calc-to-block').children('.calc-input').addClass('calc-input-active');
+});
+
+$('.calc-input').on('click', function(e){
+    $(this).removeClass('calc-input-active');
+});
