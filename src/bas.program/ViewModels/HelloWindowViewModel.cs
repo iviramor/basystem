20b4e31695d13d263a9bc41827b0b0faab23a427
Daniil <iviramor.di@gmail.com>
2021-03-29 00:56:50 +0300

    Добавлена команада завершения работы приложения

---
+++ src/bas.program/ViewModels/HelloWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using bas.program.Infrastructure.Commands;
+using bas.program.ViewModels.Base;
+
+namespace bas.program.ViewModels
+{
+    public class HelloWindowViewModel : ViewModel
+    {
+
+
+
+        #region Команды
+
+        #region Закрыть приложение
+        public ICommand CloseSystemCommand { get; }
+
+        private void OnCloseSystemCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private bool CanCloseSystemCommandExecuted(object p) => true;
+
+        #endregion
+
+        #endregion
+
+        public HelloWindowViewModel()
+        {
+            #region Команды
+
+            CloseSystemCommand = new ActionCommand(OnCloseSystemCommandExecuted, CanCloseSystemCommandExecuted);
+
+            #endregion
+        }
+
+
+
+    }
+}
