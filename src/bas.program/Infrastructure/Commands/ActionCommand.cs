722eb40f23e069e6267c791dfcd0511625e12a94
Daniil <iviramor.di@gmail.com>
2021-03-29 00:31:23 +0300

    Добавлен класс ActionCommand

---
+++ src/bas.program/Infrastructure/Commands/ActionCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bas.program.Infrastructure.Commands.Base;
+
+namespace bas.program.Infrastructure.Commands
+{
+    public class ActionCommand : Command
+    {
+
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecut;
+
+        public ActionCommand(Action<object> Execute, Func<object, bool> CanExecut = null)
+        {
+            _Execute = Execute;
+            _CanExecut = CanExecut;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecut?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
