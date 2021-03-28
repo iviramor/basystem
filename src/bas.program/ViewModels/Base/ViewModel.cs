f69d3ae53ca52f27947f8f76395b71705c0c776e
Daniil <iviramor.di@gmail.com>
2021-03-29 00:37:52 +0300

    Добавлена base ViewModel

---
+++ src/bas.program/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bas.program.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+    }
+}
