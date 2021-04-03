fb40119f4940530bfaddce550cf266ba9cf2403f
Daniil <iviramor.di@gmail.com>
2021-04-03 14:50:43 +0300

    Добавлена модель пользователя сессии

---
+++ src/bas.program.prj/Models/UserDataSession.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bas.program.Models.Tables.UserTables;
+using bas.website.Models.Data;
+
+namespace bas.program.Models
+{
+    public class UserDataSession
+    {
+
+        private BankDbContext _DataBase = new BankDbContext();
+
+        private bool _Session;
+
+        private Bank_user _User;
+
+
+        public Bank_user User
+        {
+            get => _User;
+            set
+            {
+                _User = value;
+            }
+        }
+
+        public BankDbContext DataBase
+        {
+            get => _DataBase;
+        }
+
+        public bool Session
+        {
+            get => _Session;
+            set
+            {
+                _Session = value;
+            }
+        }
+
+
+
+    }
+}
