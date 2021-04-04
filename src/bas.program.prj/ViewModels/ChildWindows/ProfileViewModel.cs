3ab5e652064cc68444279a62a026a3350167268a
Daniil <iviramor.di@gmail.com>
2021-04-04 15:53:51 +0300

    Добавлены свойства для Профиля, ограничения для ввода, Запрет на изменение.

--- src/bas.program.prj/ViewModels/ChildWindows/ProfileViewModel.cs
+++ src/bas.program.prj/ViewModels/ChildWindows/ProfileViewModel.cs
@@ -22,18 +22,147 @@ namespace bas.program.ViewModels.ChildWindows
         private ProfileWindow _ProfileWindow;
         private WorkSpaceWindowViewModel _workSpaceWindowViewModel;
 
-
-        public Bank_user _User = new Bank_user();
-        public Bank_user User
+        private bool _IsEnabled = false;
+        public bool IsEnabled
         {
-            get => _User;
+            get => _IsEnabled;
             set
             {
-                if (Equals(_User, value)) return;
-                _User = value;
+                if (Equals(_IsEnabled, value)) return;
+                _IsEnabled = value;
                 OnPropertyChanged();
             }
         }
+
+        #region Свойства пользователя
+
+        public string _Name;
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (Equals(_Name, value)) return;
+
+                if (value.Length < 2 || value.Any(ch => char.IsWhiteSpace(ch)))
+                {
+                    MessageBox.Show("Имя не может быть:\n" +
+                                    "-> Меньше 2 символов\n" +
+                                    "-> Содержать пробелы", "Ошибка ввода", MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+                    return;
+                }
+
+                _Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _Surname;
+        public string Surname
+        {
+            get => _Surname;
+            set
+            {
+                if (Equals(_Surname, value)) return;
+
+                if (value.Length < 2 || value.Any(ch => char.IsWhiteSpace(ch)))
+                {
+                    MessageBox.Show("Фамилия не может быть:\n" +
+                                    "-> Меньше 2 символов\n" +
+                                    "-> Содержать пробелы", "Ошибка ввода", MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+                    return;
+                }
+
+                _Surname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _Patronymic;
+        public string Patronymic
+        {
+            get => _Patronymic;
+            set
+            {
+                if (Equals(_Patronymic, value)) return;
+
+                if (value.Length < 2 || value.Any(ch => char.IsWhiteSpace(ch)))
+                {
+                    MessageBox.Show("Отчество не может быть:\n" +
+                                    "-> Меньше 2 символов\n" +
+                                    "-> Содержать пробелы", "Ошибка ввода", MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+                    return;
+                }
+
+                _Patronymic = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _Login;
+        public string Login
+        {
+            get => _Login;
+            set
+            {
+                if (Equals(_Login, value)) return;
+
+                if (value.Length < 6 || value.Any(ch => char.IsWhiteSpace(ch)))
+                {
+                    MessageBox.Show("Отчество не может быть:\n" +
+                                    "-> Меньше 6 символов\n" +
+                                    "-> Содержать пробелы", "Ошибка ввода", MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+                    return;
+                }
+
+                _Login = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            get => _Password;
+            set
+            {
+                if (Equals(_Password, value)) return;
+
+                if (value.Length < 5)
+                {
+                    if (value.Length < 2 || value.Any(ch => char.IsWhiteSpace(ch)))
+                    {
+                        MessageBox.Show("Пароль не может быть:\n" +
+                                        "-> Меньше 5 символов\n" +
+                                        "-> Содержать пробелы", "Ошибка ввода", MessageBoxButton.OK,
+                            MessageBoxImage.Information);
+                        return;
+                    }
+                }
+
+                _Password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _Sex;
+        public bool Sex
+        {
+            get => _Sex;
+            set
+            {
+                if (Equals(_Sex, value)) return;
+                _Sex = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        #endregion
 
         #endregion
 
@@ -48,10 +177,18 @@ namespace bas.program.ViewModels.ChildWindows
         private void OnUpdateDataCommandExecute(object p)
         {
             /// Смена имени пользователя в главом окне
-            _workSpaceWindowViewModel.UserName = User.User_name;
+            _workSpaceWindowViewModel.UserName = _Name;
 
-            /// Смена обновленных данных польоватея в сессии
-            _workSpaceWindowViewModel.User.User.ChangeUser(_workSpaceWindowViewModel.User.User, User);
+            #region Смена изменений в сессии пользователя
+
+            _workSpaceWindowViewModel.User.User.User_name = _Name;
+            _workSpaceWindowViewModel.User.User.User_surname = _Surname;
+            _workSpaceWindowViewModel.User.User.User_patronymic = _Patronymic;
+            _workSpaceWindowViewModel.User.User.User_login = _Login;
+            _workSpaceWindowViewModel.User.User.User_password = _Password;
+            _workSpaceWindowViewModel.User.User.User_sex = _Sex;
+
+            #endregion
 
             /// Изменение данных в базе данных
             _workSpaceWindowViewModel.User.DataBase.Update(_workSpaceWindowViewModel.User.User);
@@ -82,7 +219,18 @@ namespace bas.program.ViewModels.ChildWindows
         public ProfileViewModel(WorkSpaceWindowViewModel workVM)
         {
             _workSpaceWindowViewModel = workVM;
-            workVM.User.User.ChangeUser(User, workVM.User.User);
+
+            #region Значение совойств пользователя
+
+            _Name = workVM.User.User.User_name;
+            _Surname = workVM.User.User.User_surname;
+            _Patronymic = workVM.User.User.User_patronymic;
+            _Login = workVM.User.User.User_login;
+            _Password = workVM.User.User.User_password;
+            _Sex = workVM.User.User.User_sex;
+
+            #endregion
+
             UpdateDataCommand = new ActionCommand(OnUpdateDataCommandExecute, CanUpdateDataCommandExecuted);
             CloseProfileCommand = new ActionCommand(OnCloseProfileCommandExecute, CanCloseProfileCommandExecuted);
 
