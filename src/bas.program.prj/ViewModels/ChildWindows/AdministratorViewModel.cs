0ed4f09ccdb96e9c394dbf91a7d0bda07168183b
Daniil Stankevich <iviramor.di@gmail.com>
2021-04-19 15:53:44 +0300

    Добавлено ViewModel для Окна администратор

---
+++ src/bas.program.prj/ViewModels/ChildWindows/AdministratorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using bas.program.ViewModels.Base;
+using bas.program.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bas.program.ViewModels.ChildWindows
+{
+    public class AdministratorViewModel : ViewModel
+    {
+
+
+
+        public AdministratorViewModel()
+        {
+
+        }
+
+        public void ShowAdministratorWindow()
+        {
+            AdministratorWindow administratorWindow = new()
+            {
+                DataContext = this
+            };
+            administratorWindow.ShowDialog();
+        }
+    }
+}
