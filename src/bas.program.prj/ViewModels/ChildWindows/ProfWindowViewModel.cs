9467bdda2dcbd71ac43103b49727867149eb7975
Daniil Stankevich <iviramor.di@gmail.com>
2021-04-22 23:12:35 +0300

    Добавлена структура ViewModel окна Профессии

---
+++ src/bas.program.prj/ViewModels/ChildWindows/ProfWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using bas.program.ViewModels.Base;
+using bas.program.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bas.program.ViewModels.ChildWindows
+{
+    public class ProfWindowViewModel : ViewModel
+    {
+
+        #region Свойства
+
+        #region Класс
+
+        /// <summary>
+        /// Окно Свойств Профессии
+        /// </summary>
+        private ProfWindow _ProfWindow;
+
+        /// <summary>
+        /// ViewModel администратора
+        /// </summary>
+        private readonly AdministratorViewModel _AdministratorViewModel;
+
+        #endregion
+
+        #region Свойства окна
+
+        #endregion
+
+        #endregion
+
+        #region Команды
+
+
+        #endregion
+
+        public ProfWindowViewModel(AdministratorViewModel adminVM)
+        {
+            _AdministratorViewModel = adminVM;
+        }
+
+        public void ShowProfWindow()
+        {
+            _ProfWindow = new ProfWindow()
+            {
+                DataContext = this
+            };
+            _ProfWindow.ShowDialog();
+        }
+
+    }
+}
