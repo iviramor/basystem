2c4b775e69debf3e01b256ebc4706fd1a551f701
Daniil Stankevich <iviramor.di@gmail.com>
2021-04-22 11:29:51 +0300

    AdminVM - команда закрытия окна Админа. Свойство таблицы профессий

--- src/bas.program.prj/ViewModels/ChildWindows/AdministratorViewModel.cs
+++ src/bas.program.prj/ViewModels/ChildWindows/AdministratorViewModel.cs
@@ -1,21 +1,79 @@
-﻿using bas.program.ViewModels.Base;
+﻿using bas.program.Infrastructure.Commands;
+using bas.program.ViewModels.Base;
 using bas.program.Views;
+using System;
+using System.Data;
+using System.Windows.Input;
 
 namespace bas.program.ViewModels.ChildWindows
 {
     public class AdministratorViewModel : ViewModel
     {
+        /// <summary>
+        /// Окно администратора
+        /// </summary>
+        private AdministratorWindow _administratorWindow;
+
+        #region Свойства
+
+        #region Свойства элементов
+
+        private DataTable _ProfTable;
+        /// <summary>
+        /// Таблица с должностями
+        /// </summary>
+        public DataTable ProfTable
+        {
+            get
+            {
+                return _ProfTable;
+            }
+            set
+            {
+                if (Equals(_ProfTable, value)) return;
+                _ProfTable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Команды
+
+        #region Выйти из окна Админ
+
+        public ICommand CloseAdminCommand { get; }
+        private bool CanCloseAdminCommandExecuted(object p) => true;
+        private void OnNameCloseAdminCommandExecute(object p)
+        {
+            _administratorWindow.Close();
+        }
+
+        #endregion
+
+        #endregion
+
         public AdministratorViewModel()
         {
+
+            CloseAdminCommand = new ActionCommand(OnNameCloseAdminCommandExecute, CanCloseAdminCommandExecuted);
+        }
+
+
+        private void UpdateProfTable()
+        {
+
         }
 
         public void ShowAdministratorWindow()
         {
-            AdministratorWindow administratorWindow = new()
+            _administratorWindow = new()
             {
                 DataContext = this
             };
-            administratorWindow.ShowDialog();
+            _administratorWindow.ShowDialog();
         }
     }
 }
