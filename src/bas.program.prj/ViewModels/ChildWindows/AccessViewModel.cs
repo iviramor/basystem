d813e24f50f5269045d6bf308954b1f72d362d3b
Daniil Stankevich <iviramor.di@gmail.com>
2021-04-29 13:18:11 +0300

    Структура ViewModel окна Доступа

---
+++ src/bas.program.prj/ViewModels/ChildWindows/AccessViewModel.cs
@@ -0,0 +1,50 @@
+﻿using bas.program.ViewModels.Base;
+using bas.program.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bas.program.ViewModels.ChildWindows
+{
+    public class AccessViewModel : ViewModel
+    {
+
+        #region Свойства
+
+        #region Классы
+
+        /// <summary>
+        /// Окно Доступа
+        /// </summary>
+        private AccessWindow _AccessWindow;
+
+        #endregion
+
+        #endregion
+
+        #region Конструкторы
+
+        public AccessViewModel()
+        {
+
+        }
+
+        #endregion
+
+        #region Методы
+        public void ShowAccessWindow()
+        {
+            _AccessWindow = new()
+            {
+                DataContext = this
+            };
+            _AccessWindow.ShowDialog();
+            
+        }
+
+        #endregion
+
+    }
+}
