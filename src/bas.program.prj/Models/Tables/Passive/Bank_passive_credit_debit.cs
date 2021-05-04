79c34a82cac90a6ee92c1bdde0a00e0ff8e25e02
iviramor <iviramor.di@gmail.com>
2021-05-05 01:28:14 +0300

    Модель BANK_PASSIVE_CREDIT_DEBIT

--- src/bas.program.prj/Models/Tables/Passive/Bank_passive_credit_debit.cs
+++ src/bas.program.prj/Models/Tables/Passive/Bank_passive_credit_debit.cs
@@ -7,25 +7,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace bas.program.Models.Tables.Passive
 {
-    public class Bank_passive_deposits
+    public class Bank_passive_credit_debit
     {
 
         [Key]
         [DisplayName(null)]
-        public int Pas_deposit_id { get; set; }
+        public int Cdebit_id { get; set; }
 
         [DisplayName("Название депозита")]
-        public string Pas_deposit_name { get; set; }
+        public string Cdebit_name { get; set; }
+
+        [DisplayName("Название депозита")]
+        public string Cdebit_lender { get; set; }
 
         [DisplayName("Сумма")]
-        public decimal Pas_deposit_cash { get; set; }
+        public decimal Cdebit_cash { get; set; }
 
         [DisplayName(null)]
-        public int Pas_deposit_type { get; set; }
+        public int Cdebit_type { get; set; }
 
-        [ForeignKey("Pas_deposit_type")]
+        [ForeignKey("Cdebit_type")]
         [DisplayName("Тип средства(гр. Кг. Руб. Дол.)")]
         public virtual Bank_currency Bank_currency { get; set; }
-    
+
     }
 }
