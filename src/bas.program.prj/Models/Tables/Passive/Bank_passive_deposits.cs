4022cd4b04f4ac4f631131802bd1848684e6e121
iviramor <iviramor.di@gmail.com>
2021-05-05 01:22:24 +0300

    Модель BANK_PASSIVE_DEPOSITS

--- src/bas.program.prj/Models/Tables/Passive/Bank_passive_deposits.cs
+++ src/bas.program.prj/Models/Tables/Passive/Bank_passive_deposits.cs
@@ -7,25 +7,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace bas.program.Models.Tables.Passive
 {
-    public class Bank_passive_camp
+    public class Bank_passive_deposits
     {
 
         [Key]
         [DisplayName(null)]
-        public int Pcamp_id { get; set; }
+        public int Pas_deposit_id { get; set; }
 
-        [DisplayName("Название операции")]
-        public string Pcamp_name { get; set; }
+        [DisplayName("Название депозита")]
+        public string Pas_deposit_name { get; set; }
 
-        [DisplayName("Количество средства")]
-        public decimal Pcamp_quantity { get; set; }
+        [DisplayName("Сумма")]
+        public decimal Pas_deposit_cash { get; set; }
 
         [DisplayName(null)]
-        public int Pcamp_type { get; set; }
+        public int Pas_deposit_type { get; set; }
 
         [ForeignKey("Pcamp_type")]
         [DisplayName("Тип средства(гр. Кг. Руб. Дол.)")]
         public virtual Bank_currency Bank_currency { get; set; }
-
+    
     }
 }
