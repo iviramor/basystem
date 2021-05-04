af7762b2cdb0c13db2438f8c2f13a06de0487620
iviramor <iviramor.di@gmail.com>
2021-05-04 15:23:15 +0300

    Модель BANK_ACTIVE_ASSET

---
+++ src/bas.program.prj/Models/Tables/Active/Bank_active_asset.cs
@@ -0,0 +1,26 @@
+﻿using bas.website.Models.Data;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bas.program.Models.Tables.Active
+{
+    public class Bank_active_asset
+    {
+        [Key]
+        [DisplayName(null)]
+        public int Ass_id { get; set; }
+
+        [DisplayName("Название имущества")]
+        public string Ass_name { get; set; }
+
+        [DisplayName("Единица")]
+        public int Ass_cash { get; set; }
+
+        [ForeignKey("Ass_cash")]
+        [DisplayName("Тип средства(гр. Кг. Руб. Дол.)")]
+        public virtual Bank_currency Bank_currency { get; set; }
+
+
+    }
+}
