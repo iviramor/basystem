0c8205e77a3576c0a70ae630c3e5b521523ad214
iviramor <iviramor.di@gmail.com>
2021-05-04 23:21:40 +0300

    Модель BANK_PASSIVE_CAMP

---
+++ src/bas.program.prj/Models/Tables/Passive/Bank_passive_camp.cs
@@ -0,0 +1,31 @@
+﻿using bas.website.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bas.program.Models.Tables.Passive
+{
+    public class Bank_passive_camp
+    {
+
+        [Key]
+        [DisplayName(null)]
+        public int Pcamp_id { get; set; }
+
+        [DisplayName("Название операции")]
+        public string Pcamp_name { get; set; }
+
+        [DisplayName("Название средства")]
+        public decimal Pcamp_quantity { get; set; }
+
+        [DisplayName("Количественный показатель")]
+        public int Pcamp_type { get; set; }
+
+        [ForeignKey("Pcamp_type")]
+        [DisplayName("Тип средства(гр. Кг. Руб. Дол.)")]
+        public virtual Bank_currency Bank_currency { get; set; }
+
+    }
+}
