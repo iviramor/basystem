8b0f766e6ad85cac2b365abce2fd36993d989845
iviramor <iviramor.di@gmail.com>
2021-05-05 01:17:19 +0300

    Модель BANK_PASSIVE_CORRES_ACCOUTS

---
+++ src/bas.program.prj/Models/Tables/Passive/Bank_passive_corres_accouts.cs
@@ -0,0 +1,38 @@
+﻿using bas.website.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bas.program.Models.Tables.Passive
+{
+    public class Bank_passive_corres_accouts
+    {
+
+        [Key]
+        [DisplayName(null)]
+        public int Ca_bank_id { get; set; }
+
+        [DisplayName("Наименование счета")]
+        public string Ca_bank_name { get; set; }
+
+        [DisplayName(null)]
+        public int Ca_bank_company { get; set; }
+
+        [DisplayName("Сумма счет")]
+        public decimal Ca_bank_cash { get; set; }
+
+        [DisplayName(null)]
+        public int Ca_bank_type { get; set; }
+
+        [ForeignKey("Ca_bank_company")]
+        [DisplayName("Клиент")]
+        public virtual Bank_client Bank_client { get; set; }
+
+        [ForeignKey("Ca_bank_type")]
+        [DisplayName("Тип средства(гр. Кг. Руб. Дол.)")]
+        public virtual Bank_currency Bank_currency { get; set; }
+
+    }
+}
