f9b201a51bd2cd2c9716959731ba88ca243bc2a5
iviramor <iviramor.di@gmail.com>
2021-05-05 02:27:43 +0300

    Создано окно для BankClientHistory и VM(Только показ)

---
+++ src/bas.program.prj/ViewModels/DialogViewModels/EditorsDialogWindow/BankClientHistoryViewModel.cs
@@ -0,0 +1,31 @@
+﻿using bas.program.ViewModels.Base;
+using bas.program.Views.DialogViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bas.program.ViewModels.DialogViewModels.EditorsDialogWindow
+{
+    public class BankClientHistoryViewModel : ViewModel
+    {
+
+        public BankClientHistoryViewModel()
+        {
+                
+        }
+
+
+        public void ShowBankClientWindow()
+        {
+            new BankClientHistoryWindow()
+            {
+                DataContext = this
+            }.ShowDialog();
+            
+        }
+
+
+    }
+}
