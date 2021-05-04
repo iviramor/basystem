ce321ce08f5d4b01231c28d1c6b4cc24a18c60dc
iviramor <iviramor.di@gmail.com>
2021-05-04 15:07:33 +0300

    Модель BANK_ACTIVE_DEPOSITS

---
+++ src/bas.program.prj/Models/Tables/Active/Bank_active_deposits.cs
@@ -0,0 +1,33 @@
+﻿using bas.website.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bas.program.Models.Tables.Active
+{
+    public class Bank_active_deposits
+    {
+
+        [Key]
+        [DisplayName(null)]
+        public int Act_deposit_id { get; set; }
+
+        [DisplayName("Название депозита")]
+        public string Act_deposit_name { get; set; }
+
+        [DisplayName("Сумма")]
+        public decimal Act_deposit_cash { get; set; }
+
+        [DisplayName(null)]
+        public decimal Act_deposit_type { get; set; }
+
+        [ForeignKey("Act_deposit_type")]
+        [DisplayName("Тип средства(гр. Кг. Руб. Дол.)")]
+        public virtual Bank_currency Bank_currency { get; set; }
+    }
+}
