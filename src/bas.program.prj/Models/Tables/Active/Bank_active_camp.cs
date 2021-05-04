cae0529e2a3601987a753b0486d0912c53c6557e
iviramor <iviramor.di@gmail.com>
2021-05-04 12:54:52 +0300

    Модель BANK_ACTIVE_CAMP

---
+++ src/bas.program.prj/Models/Tables/Active/Bank_active_camp.cs
@@ -0,0 +1,34 @@
+﻿using bas.website.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bas.program.Models.Tables.Active
+{
+    public class Bank_active_camp
+    {
+
+        [Key]
+        [DisplayName(null)]
+        public int Acamp_id { get; set; }
+
+        [DisplayName("Название средства")]
+        public string Acamp_name { get; set; }
+
+        [DisplayName("Количественный показатель")]
+        public string Acamp_quantity { get; set; }
+
+        [DisplayName(null)]
+        public string Acamp_type { get; set; }
+
+        [ForeignKey("Acamp_type")]
+        [DisplayName("Валюта")]
+        public virtual Bank_currency Bank_currency { get; set; }
+
+    }
+}
