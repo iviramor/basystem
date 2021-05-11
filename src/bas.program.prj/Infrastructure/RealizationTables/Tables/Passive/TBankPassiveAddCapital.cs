bd10125dd2d2533866d17dc0d1f9b329ec0573e0
iviramor <iviramor.di@gmail.com>
2021-05-11 07:52:45 +0300

    Готов BankPassiveAddCapital

---
+++ src/bas.program.prj/Infrastructure/RealizationTables/Tables/Passive/TBankPassiveAddCapital.cs
@@ -0,0 +1,158 @@
+﻿using bas.program.Infrastructure.RealizationTables.Base;
+using bas.program.Models.Tables.Passive;
+using bas.program.Models.Tables.UserTables;
+using bas.program.ViewModels;
+using bas.program.ViewModels.DialogViewModels.EditorsDialogWindow.Passive;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace bas.program.Infrastructure.RealizationTables.Tables.Passive
+{
+    public class TBankPassiveAddCapital : ATable
+    {
+
+        #region Свойства
+
+        private Bank_passive_add_capital Bank_data;
+
+        #endregion Свойства
+
+        #region Элементы главного окна
+
+        public override bool Filter { get; } = false;
+        public override bool Maths { get; } = false;
+
+        #endregion Элементы главного окна
+
+        #region Методы
+
+        #region Работа с таблицей
+
+        /// <summary>
+        /// Заполняет таблицу данными
+        /// </summary>
+        private void SetValuesTable()
+        {
+
+            UpdateDBContext();
+
+            var data = BankDbContext.Bank_passive_add_capital
+                .Include(a => a.Bank_currency)
+                .ToList();
+
+            DataTable.Clear();
+
+            foreach (var item in data)
+                DataTable.Rows.Add(
+                    item.Addc_name,
+                    item.Addc_descrip,
+                    item.Addc_debit,
+                    item.Addc_credit,
+                    item.Bank_currency.Currency_name
+                    );
+        }
+
+        #endregion
+
+        #endregion Методы
+
+        #region Команды
+
+        #region Удалить
+
+        public override void OnRemoveCommandExecute(object p)
+        {
+            if (HasNullObject()) return;
+
+            if (CheckUserPassword())
+            {
+                BankDbContext.Bank_passive_add_capital.Remove(Bank_data);
+                BankDbContext.SaveChanges();
+                MessageBox.Show($"{Bank_data.Addc_name} - Удалено");
+                UpdateDataInTable();
+                return;
+            }
+
+        }
+
+        #endregion Удалить
+
+        #region Добавить
+
+        public override void OnAddCommandExecute(object p)
+        {
+            BankPassiveAddCapitalViewModel clientViewModel = new(workSpaceWindowViewModel, "Добавить");
+            clientViewModel.ShowWindow();
+        }
+
+        #endregion Добавить
+
+        #region Изменить
+
+        public override void OnEditCommandExecute(object p)
+        {
+            if (HasNullObject()) return;
+
+            BankPassiveAddCapitalViewModel clientViewModel = new(workSpaceWindowViewModel, Bank_data);
+            clientViewModel.ShowWindow();
+        }
+
+        #endregion Изменить
+
+        #region Просмотр
+
+        public override void OnShowCommandExecute(object p)
+        {
+            if (HasNullObject()) return;
+
+            BankPassiveAddCapitalViewModel clientViewModel = new(Bank_data, workSpaceWindowViewModel.User.DataBase);
+            clientViewModel.ShowWindow();
+        }
+
+        #endregion
+
+        #endregion
+
+        public override void SetSelected(DataRowView selectedItem)
+        {
+            if (selectedItem == null)
+            {
+                Bank_data = null;
+                return;
+            }
+
+            Bank_data = BankDbContext.Bank_passive_add_capital
+                .SingleOrDefault(item =>
+                            item.Addc_name == (string)selectedItem[0] &&
+                            item.Addc_descrip == (string)selectedItem[1]);
+        }
+
+        public override DataTable GetFullTable()
+        {
+            SetValuesTable();
+            return DataTable;
+        }
+
+        public override bool HasNullObject()
+        {
+            if (Bank_data == null)
+            {
+                ShowNullObjectError();
+                return true;
+            }
+            return false;
+        }
+
+        public TBankPassiveAddCapital(Bank_user_access bank_User_Access, WorkSpaceWindowViewModel workVM) : base(bank_User_Access, workVM)
+        {
+
+        }
+
+    }
+}
