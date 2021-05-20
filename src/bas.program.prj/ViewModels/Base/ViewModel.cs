48774a672a1fdcae9dbd4e04a1ec643e54bf4a54
iviramor <iviramor.di@gmail.com>
2021-05-20 12:23:47 +0300

    Описание

--- src/bas.program.prj/ViewModels/Base/ViewModel.cs
+++ src/bas.program.prj/ViewModels/Base/ViewModel.cs
@@ -3,6 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace bas.program.ViewModels.Base
 {
+    /// <summary>
+    /// Класс реализующий OnPropertyChanged для свойств
+    /// </summary>
     public abstract class ViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
